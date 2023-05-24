4bdd7aada2704cb3c75d89be24390cac6a8c4ae9
Bahareh <mer_ir@yahoo.com>
2023-05-24 16:05:51 -0700

    add indexedDB methods, add configurations in webpack.config.js, fixed src-sw.js, and change install.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,19 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
-/*registerRoute(
-  // Match the assets you want to cache
-  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
-  new CacheFirst({
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60, // Adjust the cache duration as needed
-      }),
     ],
   })
-);*/
+);
